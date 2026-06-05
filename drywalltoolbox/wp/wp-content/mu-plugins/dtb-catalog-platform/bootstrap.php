0df63781b938b161bd82e85b17d2dd68d32bc0d8
Elliott Miller <elliotttmiller@hotmail.com>
2026-06-05 16:42:20 -0500

    fix(inventory): include inventory REST controller in bootstrap

--- drywalltoolbox/wp/wp-content/mu-plugins/dtb-catalog-platform/bootstrap.php
+++ drywalltoolbox/wp/wp-content/mu-plugins/dtb-catalog-platform/bootstrap.php
@@ -71,6 +71,7 @@ require_once $_dtb_cp . '/Rest/CompatiblePartsController.php';
 require_once $_dtb_cp . '/Rest/ToolsetTemplatesController.php';
 require_once $_dtb_cp . '/Rest/ToolsetOptionsController.php';
 require_once $_dtb_cp . '/Rest/ToolsetValidationController.php';
+require_once $_dtb_cp . '/Rest/InventoryIntelligenceController.php';
 
 // Validation.
 require_once $_dtb_cp . '/Validation/CatalogValidationService.php';
