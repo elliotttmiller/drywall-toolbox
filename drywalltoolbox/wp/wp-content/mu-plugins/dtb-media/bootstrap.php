88c4b57a3064dab90047aae3e0c8aef532042c8b
Elliott Miller <elliotttmiller@hotmail.com>
2026-07-12 04:05:32 -0500

    Load variation gallery resolver from dtb-media

--- drywalltoolbox/wp/wp-content/mu-plugins/dtb-media/bootstrap.php
+++ drywalltoolbox/wp/wp-content/mu-plugins/dtb-media/bootstrap.php
@@ -27,6 +27,7 @@ dtb_module_require( 'dtb-media/Application/PurgeUnlinkedImages.php' );
 dtb_module_require( 'dtb-media/Application/ReleaseImageSyncLock.php' );
 dtb_module_require( 'dtb-media/Validation/ImageMimeValidator.php' );
 dtb_module_require( 'dtb-media/Infrastructure/ImageSyncRepository.php' );
+dtb_module_require( 'dtb-media/Services/VariationGalleryResolver.php' );
 dtb_module_require( 'dtb-media/Infrastructure/RemoteImageFetcher.php' );
 dtb_module_require( 'dtb-media/Application/RegisterProductImages.php' );
 dtb_module_require( 'dtb-media/Services/ProductImageLinker.php' );
