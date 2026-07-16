46c4a9232b879f68fbf109737038e7c523f7eada
Elliott Miller <elliotttmiller@hotmail.com>
2026-07-15 19:04:41 -0500

    Load checkout Blocks capability detector

--- drywalltoolbox/wp/wp-content/mu-plugins/dtb-commerce/bootstrap.php
+++ drywalltoolbox/wp/wp-content/mu-plugins/dtb-commerce/bootstrap.php
@@ -23,6 +23,7 @@ require_once __DIR__ . '/Payment/UnpaidOrderPayGuard.php';
 require_once __DIR__ . '/Payment/PaymentBnplCartFinalization.php';
 require_once __DIR__ . '/Payment/CustomerAssociation.php';
 require_once __DIR__ . '/Payment/PaymentStatusGuard.php';
+require_once __DIR__ . '/Payment/CheckoutBlocksCapabilityDetector.php';
 require_once __DIR__ . '/Shipping/DTBShippingMethod.php';
 require_once __DIR__ . '/Rest/CheckoutRestController.php';
 require_once __DIR__ . '/Email/WooCommerceBrandedEmails.php';
