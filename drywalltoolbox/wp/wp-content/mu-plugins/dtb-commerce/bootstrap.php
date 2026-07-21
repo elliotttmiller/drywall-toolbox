55f4e10f23a92a833a426565dc4c01c0ecf8e267
Elliott Miller <elliotttmiller@hotmail.com>
2026-07-20 21:08:52 -0500

    Load checkout performance hardening

--- drywalltoolbox/wp/wp-content/mu-plugins/dtb-commerce/bootstrap.php
+++ drywalltoolbox/wp/wp-content/mu-plugins/dtb-commerce/bootstrap.php
@@ -22,6 +22,7 @@ require_once __DIR__ . '/Payment/WooNativeCheckoutRuntime.php';
 require_once __DIR__ . '/Payment/StorefrontReturnContext.php';
 require_once __DIR__ . '/Payment/OfficialStripeNativeCheckout.php';
 require_once __DIR__ . '/Payment/MobilePaymentSheet.php';
+require_once __DIR__ . '/Payment/CheckoutPerformance.php';
 require_once __DIR__ . '/Shipping/DTBShippingMethod.php';
 require_once __DIR__ . '/Email/WooCommerceBrandedEmails.php';
 require_once __DIR__ . '/Email/WooCommerceAdminBrandedEmails.php';
